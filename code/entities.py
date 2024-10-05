a14cccea71450be8d7637fff09ac149252fee622
Kyle Martin <kyle.martin.0819@gmail.com>
2024-10-05 16:40:22 -0400

    Move player sprite
    Update input method to have the key pressed update the vector of the sprite then update the global vector variable.
    
    In the move method, manipulate the objects center based off the direction of the vector multiplied by the speed or velocity.
    
    Call the move method in the pdate method.

--- code/entities.py
+++ code/entities.py
@@ -7,23 +7,25 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((100, 100))
         self.image.fill('red')
         self.rect = self.image.get_frect(center = pos)
+        
+        self.direction = vector()
     
     def input(self):
         keys = pygame.key.get_pressed()
+        input_vector = vector()
         if keys[pygame.K_UP]:
-            print('Up')
-            
+            input_vector.y -= 1
         if keys[pygame.K_DOWN]:
-            print('Down')
-            
+            input_vector.y += 1
         if keys[pygame.K_LEFT]:
-            print('Left')
-            
+            input_vector.x -= 1
         if keys[pygame.K_RIGHT]:
-            print('Right')
+            input_vector.x += 1
+        self.direction = input_vector
     
-    def move(self, dt):
-        pass
+    def move(self):
+        self.rect.center += self.direction * 2
     
     def update(self):
         self.input()
+        self.move()
