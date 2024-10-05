6ffa8c2e485ae935f957f2ef21d61c804ef0d7c9
Kyle Martin <kyle.martin.0819@gmail.com>
2024-10-05 15:21:08 -0400

    Import a tiled map file
    use os.path to not hard code file paths as some os's use backslashes and some use forward slashes.

--- code/main.py
+++ code/main.py
@@ -1,4 +1,8 @@
 from settings import *
+from os.path import join
+
+# pip install pytmx
+from pytmx.util_pygame import load_pygame
 
 class Game:
     def __init__(self):
@@ -6,6 +10,12 @@ class Game:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('PokePython')
         
+        self.import_assets()
+    
+    def import_assets(self):
+        self.tmx_maps = {'world': load_pygame(join('data', 'maps', 'world.tmx'))}
+        
+    
     def run(self):
         while True:
             for event in pygame.event.get():
